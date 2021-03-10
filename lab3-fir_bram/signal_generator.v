67d524a73e6f4e88f079e58d2c5a907f572832f8
Gonzalo G. Fernández <fernandez.gfg@gmail.com>
2021-03-10 16:02:22 -0300

    [new] lab3 signal mux working

---
+++ lab3-fir_bram/signal_generator.v
@@ -0,0 +1,55 @@
+/*
+ *  Module `signal_generator`
+ *
+ *  Generacion de señal a partir de archivo con 1024 valores.
+ *
+ *  Modificaciones: Ruta de archivo vacia, por lo que es necesario
+ *  modificar parametro MEM_INIT_FILE con el archivo a general 
+ *  al instanciar el modulo. Eliminacion de parametro NB_SEL.
+ *
+ *  EAMTA 2021 - Digital Design
+ *  Laboratorio 3: Integracion filtro FIR con memoria BRAM.
+ *  Gonzalo G. Fernandez     10/03/2021
+ *  File: signal_generator.v
+ */
+
+module signal_generator(
+        i_clock,
+        i_reset, 
+        o_signal
+    );
+    
+// Parameters
+parameter NB_DATA   = 8;
+parameter NB_COUNT  = 10;
+parameter MEM_INIT_FILE = "";
+
+// Ports
+input                           i_clock;
+input                           i_reset;
+output signed [NB_DATA-1 : 0]   o_signal;
+
+// Vars
+reg [NB_COUNT-1 : 0]    counter;
+reg [NB_DATA-1 : 0]     data[1023:0];
+
+// Signal file read
+initial begin
+    if (MEM_INIT_FILE != "") begin
+        $readmemh(MEM_INIT_FILE, data);
+    end    
+end
+
+// Counter logic
+always @(posedge i_clock or posedge i_reset) begin
+    if (i_reset) begin
+        counter <= {NB_COUNT{1'b0}};
+    end
+    else begin
+        counter <= counter + {{NB_COUNT-1{1'b0}}, {1'b1}};
+    end
+end
+
+assign o_signal = data[counter];
+    
+endmodule
---
+++ lab3-fir_bram/tb_signal_mux.v
@@ -0,0 +1,71 @@
+/*
+ *  Module `tb_signal_mux`
+ *
+ *  Testbench del multiplexor de señales generadas.
+ *
+ *  EAMTA 2021 - Digital Design
+ *  Laboratorio 3: Integracion filtro FIR con memoria BRAM.
+ *  Gonzalo G. Fernandez     10/03/2021
+ *  File: tb_signal_mux.v
+ */
+
+`timescale 1ns/100ps
+
+module tb_signal_mux();
+
+// Parameters
+parameter NB_DATA   = 8;
+parameter NB_SEL    = 2;
+
+// Vars
+reg                             clock;      // Señal de clock
+reg                             reset;      // Señal de reset
+reg            [NB_SEL-1 : 0]   sel;        // Seleccion de señal
+wire    signed [NB_DATA-1 : 0]  w_signal;   // Salida de multiplexor
+
+// Señales generadas
+wire    signed [NB_DATA-1 : 0]  w_signal_1;
+wire    signed [NB_DATA-1 : 0]  w_signal_2;
+wire    signed [NB_DATA-1 : 0]  w_signal_3;
+wire    signed [NB_DATA-1 : 0]  w_signal_4;
+
+assign w_signal_1 = tb_signal_mux.u_signal_mux.w_signal_1;
+assign w_signal_2 = tb_signal_mux.u_signal_mux.w_signal_2;
+assign w_signal_3 = tb_signal_mux.u_signal_mux.w_signal_3;
+assign w_signal_4 = tb_signal_mux.u_signal_mux.w_signal_4;
+
+// Simulacion
+initial begin
+    // Condiciones iniciales
+    reset   = 1'b0;
+    sel     = 2'b00;
+    clock   = 1'b1; 
+    
+    #100    reset   = 1'b1;   // Abandonar condicion de reset
+    
+    // Test de seleccion de señal
+    #1000   sel     = 2'b01;
+    #1000   sel     = 2'b10;
+    #1000   sel     = 2'b11;
+    
+    #1000 $finish;
+end
+
+// Generacion de onda de clock (100MHz)
+always
+    #5 clock = ~clock;
+
+signal_mux #
+    (
+    .NB_DATA(NB_DATA),
+    .NB_SEL(NB_SEL)
+    )
+u_signal_mux
+    (
+    .i_clock(clock),
+    .i_reset(~reset),
+    .i_sel(sel),
+    .o_signal(w_signal)
+    );
+
+endmodule
